dfefd24dd13cf3c18aa30f6ad48344b610167d49
mayankmohak <mayank8199@gmail.com>
2021-06-10 21:42:52 +0530

    add resume

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -28,13 +28,13 @@ class Home extends Component {
                 <div className="about-me">
                     <div className="about-me__row">
                         <div className="about-me__col-description">
-                            <p>I am currently pursuing <strong>Bachelors of Technology</strong> in the field of Computer Science and Engineering. I am skilled in domain of <strong>Frontend Development</strong>, <strong>Backend Development</strong>, <strong>Problem Solving</strong> and <strong>Data Structure & Algorithms</strong>.</p> 
+                            <p>Hi, I am currently pursuing <strong>Bachelors of Technology</strong> in the field of Computer Science and Engineering. I am skilled in domain of <strong>Frontend Development</strong>, <strong>Backend Development</strong>, <strong>Problem Solving</strong> and <strong>Data Structure & Algorithms</strong>.</p> 
                             {/* I regularly do <strong>Competitive Coding</strong> and regular at <strong><a href="https://www.codechef.com/users/mayankmohak">CodeChef</a></strong> and <strong><a href="http://codeforces.com/profile/mayankmohak">Codeforces</a></strong>. On <strong><a href="https://www.hackerrank.com/mayankmohak">HackerRank</a></strong> completed <strong>C</strong> and <strong>JAVA</strong> language Problem Set.</p> */}
 
                             <p>I am a <strong>Team Player</strong> and love Team Work.</p>
 
                             {/* <p>In Co-Curricular Activities I am a good chess player and won awards at inter-school level. Outdoor Sports are my first Preferance. </p> */}
-                            
+                            <div className="download-resume"><a href="../../../resume.pdf">RESUME</a></div>
                         </div>
                         <div className="about-me__col-image">
                             <img src={profile} alt="my-profile" className="about-me__col-image__profile-image" />
