aa671d407ed1f41b6b53e76b8d42ffe945855b60
mayankmohak <mayank8199@gmail.com>
2021-08-03 13:54:06 +0530

    adding contact form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,21 @@
+import emailjs from "emailjs-com"
 import React from "react"
 
 import "./Contact.css"
 
 function Contact() {
+
+  function sendEmail(e){
+    e.preventDefault();
+
+    emailjs.sendForm('service_gdgjyog', 'template_i9e2uz2', e.target, 'user_ZgoP0IomBr2CKB8rWe3f6')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
     <React.Fragment>
 
@@ -10,14 +23,14 @@ function Contact() {
       <section id="contact" data-animate="bounceIn" className="contact-section contact">
         <br />
         <br />
-        <p className="title"> &nbsp; Contact me</p>
-        <form id="contact-form" className="col-md-6 mx-auto" method="post" action="#">
+        <p className="title"> &nbsp; Contact me !!!</p>
+        <form id="contact-form" className="col-md-6 mx-auto" onSubmit={sendEmail}>
           <div className="controls">
             <div className="col-md-6 d-inline-block">
-              <input type="text" name="name" placeholder="Name *" required className="form-control" />
+              <input type="text" name="name" placeholder="Your Name *" required className="form-control" />
             </div>
             <div className="col-md-6 d-inline-block">
-              <input type="text" name="email" placeholder="Email *" required className="form-control" />
+              <input type="text" name="email" placeholder="Your Email *" required className="form-control" />
             </div>
             <div className="col-md-12">
               <textarea name="message" placeholder="Message for me *" rows="4" required className="form-control"></textarea>
